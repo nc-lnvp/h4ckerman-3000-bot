9a42b8b1cd0b334e66844e188a4f47b1b9ef07e5
nc-lnvp <nn9262186@gmail.com>
2021-05-12 21:33:10 -0700

    oops forgot the token

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 import discord
+import base64
+import random
 
 client = discord.Client()
+
+epic_hacking = [
+    "https://tenor.com/view/pc-hack-hacker-guy-fawkes-mask-gif-17047235",
+    "https://tenor.com/view/typing-fast-cyber-banana-help-gif-16125910",
+    "https://tenor.com/view/mega64-hacking-in-progress-hacker-hacked-hd-gif-16542434",
+    "https://tenor.com/view/hacker-game-over-laptop-hacking-hack-gif-17366041",
+    "https://cdn.discordapp.com/attachments/670767796925235252/842257131142250516/breenhacking.webm"
+]
 
 @client.event
 async def on_ready():
@@ -11,7 +21,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!hack'):
+        await message.channel.send(random.choice(epic_hacking))
 
-client.run('your token here')
+token = base64.b64decode(b'T0RReU1qUTNPRFl6TWpVek56STVNamt3LllKeWljZy43S0c5MzRWRWxtM1J0Wm45YlVhQ0xTdnJPeUk=').decode()
+client.run(token)
